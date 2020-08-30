fd0268bc6bd6fb01199d20e9e8fab7951ad49e68
hema1909 <70359962+hema1909@users.noreply.github.com>
2020-08-30 16:43:04 +0530

    Add files via upload

---
+++ Sub_Num.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class Sub_Num {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = a-b;
+		System.out.println("sub of 2 numbers = "+c);
+
+	}
+
+}
