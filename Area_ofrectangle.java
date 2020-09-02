3130b65561dc6f3bd39bfecbb49e4124a9788bf0
hema1909 <70359962+hema1909@users.noreply.github.com>
2020-09-02 13:45:21 +0530

    Add files via upload

---
+++ Area_ofrectangle.java
@@ -0,0 +1,15 @@
+package pack1;
+import java.util.*;
+public class Area_ofrectangle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter length:");
+		int l = sc.nextInt();
+		System.out.println("enter breadth:");
+		int b = sc.nextInt();
+		int a = l*b;
+		System.out.println("area of rectangle = "+a);
+	}
+
+}
