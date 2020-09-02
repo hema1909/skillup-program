e1fadd3d16c759741844051953cb6ce0674db648
hema1909 <70359962+hema1909@users.noreply.github.com>
2020-09-02 21:20:15 +0530

    Add files via upload

---
+++ Area_ofcircle.java
@@ -0,0 +1,17 @@
+package pack1;
+import java.util.*;
+public class Area_ofcircle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("enter radius:");
+		float r = sc.nextFloat();
+		float a = 3.14f * r * r;
+		System.out.println("area of circle="+a);
+		System.out.println(a);
+		
+		
+
+	}
+
+}
