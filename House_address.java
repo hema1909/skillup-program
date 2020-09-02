c03a1dfe2736ba65fdad40e4b3e0d3ee8492b3b8
hema1909 <70359962+hema1909@users.noreply.github.com>
2020-09-02 13:43:41 +0530

    Add files via upload

---
+++ House_address.java
@@ -0,0 +1,14 @@
+package pack1;
+
+public class House_address {
+
+	public static void main(String[] args){
+		String name = "suresh";
+        String hno = "10/1";
+        String city = "surampalem";
+        System.out.println("name = "+name);
+        System.out.println("hno = "+hno);
+        System.out.println("city ="+city);
+	}
+
+}
