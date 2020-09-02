878b4255f1a14a81ee057a25743dbfd32c000453
hema1909 <70359962+hema1909@users.noreply.github.com>
2020-09-02 21:21:13 +0530

    Add files via upload

---
+++ Circumference_area.java
@@ -0,0 +1,12 @@
+package pack1;
+import java.util.*;
+public class Circumference_area {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter radius:");
+	
+
+	}
+
+}
