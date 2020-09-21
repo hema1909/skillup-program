7dbf8306e4bbbac6aed25926e4a90a5e1d056967
hema1909 <70359962+hema1909@users.noreply.github.com>
2020-09-21 18:44:56 +0530

    Add files via upload

---
+++ For1_10.java
@@ -0,0 +1,15 @@
+package pack2;
+
+public class For1_10
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("numbers from 1 to 10:");
+		for(int i=1; i<=10; i++)
+		{
+			System.out.print(i+"");
+		}
+	}
+
+}
