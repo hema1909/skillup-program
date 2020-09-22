ddcd9f7105262ae443bd512f40063e509271631e
hema1909 <70359962+hema1909@users.noreply.github.com>
2020-09-22 15:48:21 +0530

    Add files via upload

---
+++ Multiple_loops.java
@@ -0,0 +1,17 @@
+package pack2;
+
+public class Multiple_loops {
+
+	public static void main(String[] args) {
+		for(int j=1;j<=5;j++) //rows
+		{
+			for(int i=1;i<=5;i++) //cols
+			{
+				System.out.print(i+"\t");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
