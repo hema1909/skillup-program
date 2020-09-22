25eb26f3e9ee64fc04095eb2e311ef77eca568ed
hema1909 <70359962+hema1909@users.noreply.github.com>
2020-09-22 16:33:22 +0530

    Add files via upload

---
+++ Even_1_20.java
@@ -0,0 +1,14 @@
+package pack2;
+
+public class Even_1_20 {
+
+	public static void main(String[] args) {
+	 System.out.println("even numbers from 1 to 20");
+	 for(int i=1; i<=20; i++) // cols
+	 {
+		 if(i%2==0)
+			 System.out.println(i);
+	 }
+	}
+
+}
