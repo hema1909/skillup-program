354391ad79f22e2421f7b7e8830c1a00c6e72860
hema1909 <70359962+hema1909@users.noreply.github.com>
2020-09-22 16:34:15 +0530

    Add files via upload

---
+++ While1_10.java
@@ -0,0 +1,15 @@
+package pack2;
+
+public class While1_10 {
+
+	public static void main(String[] args) {
+	System.out.println("numbers from 1 to 10");
+	int i=1;
+	while(i<=10)
+	{
+		System.out.println(i);
+		i=i+1;
+	}
+	}
+
+}
