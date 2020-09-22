804dab8d1f58ccd5e2f24700121d8834d087381e
hema1909 <70359962+hema1909@users.noreply.github.com>
2020-09-22 16:30:26 +0530

    Add files via upload

---
+++ Multiple_loops2.java
@@ -0,0 +1,18 @@
+package pack2;
+
+public class Multiple_loops2 {
+
+	public static void main(String[] args) {
+		System.out.println("even numbers from 1 to 20");
+		for(char i='A'; i<='E'; i++)
+		{
+			for(char j='A'; j<='E'; j++)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
