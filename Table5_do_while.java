43f6cb8889e097a8e9bd3845bc04fbbc95e5a149
hema1909 <70359962+hema1909@users.noreply.github.com>
2020-09-23 21:41:36 +0530

    Add files via upload

---
+++ Table5_do_while.java
@@ -0,0 +1,17 @@
+package pack2;
+
+public class Table5_do_while {
+
+	public static void main(String[] args) {
+		System.out.println("5 table:");
+		int i=1;
+		int n=5;
+		do
+		{
+			System.out.println(n + " * " + i + " = " +(n*i));
+			i=i+1;
+		}
+		while(i<=10);
+	}
+
+}
