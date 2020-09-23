16c466bf4415a09f6a5709a0d0ad561959eba29a
hema1909 <70359962+hema1909@users.noreply.github.com>
2020-09-23 21:38:51 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,14 @@
+package pack2;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int a[] ={10,20,30,40,50};
+		System.out.println("Array values are:");
+        for( int i=0; i<5; i++)
+        {
+        	System.out.println(a[i]);
+        }
+	}
+
+}
