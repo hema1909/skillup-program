70b32339b5acbda28dc50340acec4cd001e0663e
hema1909 <70359962+hema1909@users.noreply.github.com>
2020-09-23 21:41:02 +0530

    Add files via upload

---
+++ Table_5.java
@@ -0,0 +1,17 @@
+package pack2;
+
+public class Table_5 {
+
+	public static void main(String[] args) {
+		System.out.println("5 table");
+		int i=1;
+		int n=5;
+		while(i<=10)
+		{
+			System.out.println(n + " * " + i + " = " +(n*i));
+			i=i+1;
+		}
+
+	}
+
+}
